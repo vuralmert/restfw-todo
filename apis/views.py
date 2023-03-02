e562cc08709da3f17988ea0c80380799ad72a794
Mert <100792959+vuralmert@users.noreply.github.com>
2023-03-02 12:08:42 +0300

    Added generic views, List and Detail

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from todos import models
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class ListTodo(generics.ListCreateAPIView): #Eklenen todolari gormek icin eklenen generic view
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView): #Todolar uzerindeki diger cbv icin generic view
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
