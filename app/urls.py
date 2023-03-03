9956118dfd6b6858a0317dbd2515072a24902871
Mert <100792959+vuralmert@users.noreply.github.com>
2023-03-03 17:14:11 +0300

    Since the library I am trying to use is no longer supported, changed rest_framework_swagger to drf_yasg

--- app/urls.py
+++ app/urls.py
@@ -15,13 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-schema_view = get_swagger_view(title='Todo App')
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo App",
+        default_version='v1.0',
+        description="Mert Vural - Todo App",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
-    path('docs/', schema_view),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
