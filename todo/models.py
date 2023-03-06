98e25c50ef59c93ce9110840a4b43b85bd8aaf91
Mert <100792959+vuralmert@users.noreply.github.com>
2023-03-06 13:05:08 +0300

    Added new features to todos

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 
 class Todo(models.Model):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    PRIORITY_CHOICES = ((LOW, 'Low'),(MEDIUM, 'Medium'), (HIGH, 'High'))
+
     title = models.CharField(max_length=200)
     id = models.IntegerField(primary_key=True)
     description = models.TextField()
+    deadline = models.DateTimeField(null=True, blank=True)
+    priority = models.IntegerField(default=1, choices=PRIORITY_CHOICES)
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
+from django.utils import timezone
 from todo import models
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
+        model = models.Todo
         fields = (
             'id',
             'title',
             'description',
+            'deadline',
+            'priority',
+            'is_done',
         )
-        model = models.Todo
+
+
+
+    def validate_deadline(self,value):
+        if (timezone.now() > value):
+            raise serializers.ValidationError('Deadline must be in the future.')
+        return value
